6fe9cce9c5d1f1a47182f99b1ce83e5113751953
himanshurajput8 <rajputhimanshu913@gmail.com>
2026-01-20 09:05:56 +0530

    aad details

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -12,8 +12,8 @@ export function FooterComponent(){
                 </div>
                 <div>
                     <ul>
-                        <h3>Zomaverse</h3>
-                        <li> Feedind India</li>
+                        <h3> Zomaverse</h3>
+                        <li>Feedind India</li>
                         <li>Hyperpure</li>
                         <li>Weather Union</li>
                     </ul>
